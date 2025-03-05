082e5e67efe35c751492664780f39f667283fc99
Aytekin <aytkinvliyeva32@gmail.com>
2025-03-05 19:07:13 +0400

    7-ci kommit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,26 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     screens: {
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
-
+      'sm': '500px',
+     
       'md': '768px',
-      // => @media (min-width: 768px) { ... }
-
+      
       'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
+     
       'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
 
       '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
+    },
+    theme: {
+      container: {
+        padding:'2 rem',
+          screens: {
+            sm: '100%',  
+            md: '80%',   
+            lg: '1024px',  
+            xl: '1200px',  
+        },
+      },
     },
     extend: {},
   },
